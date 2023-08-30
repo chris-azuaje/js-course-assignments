3fd8bc9137dcaa9fbad64b0fa9026b5ac1dca782
Chris Azuaje <christopherazuaje@gmail.com>
2023-08-30 18:45:00 -0400

    Assignment 01

---
+++ assignment-01.js
@@ -0,0 +1,8 @@
+let country = 'USA';
+let continent = 'North America';
+let population = 331000000;
+
+// Visible through Quokka extension
+console.log(country);
+console.log(continent);
+console.log(population);
